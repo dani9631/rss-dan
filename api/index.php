c25b847d22983c95da84c5dd72a219b5d9dda103
dani9631 <49661197+dani9631@users.noreply.github.com>
2026-09-19 13:41:02 +0200

    Redirect to main index.php file

--- api/index.php
+++ api/index.php
@@ -1,11 +1,1 @@
-<?php
-// Carica l'ambiente interno e le classi di RSS-Bridge
-require __DIR__ . '/../lib/bootstrap.php';
-
-// Ora la classe Configuration esiste e possiamo forzare i parametri per Vercel
-Configuration::set('cache', 'type', 'file');
-Configuration::set('cache', 'path', '/tmp/cache/');
-
-// Avvia l'applicazione principale
-$app = new RssBridge();
-$app->main();
+<?php require __DIR__ . '/../index.php';
