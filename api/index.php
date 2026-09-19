59d4669a624712c9c1bc060a88a1a1c973973a89
dani9631 <49661197+dani9631@users.noreply.github.com>
2026-09-19 13:46:37 +0200

    Define absolute data path for Vercel

--- api/index.php
+++ api/index.php
@@ -1,1 +1,6 @@
-<?php require __DIR__ . '/../index.php';
+<?php
+// Definisce in modo assoluto la cartella dei dati nell'unica directory scrivibile di Vercel
+define('PATH_DATA', '/tmp/');
+
+// Carica il core di RSS-Bridge
+require __DIR__ . '/../index.php';
